93b052ddacdee11a83484e262f41090229f51185
iamptic <65834099+iamptic@users.noreply.github.com>
2025-08-19 12:16:38 +0300

    Add files via upload

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, Date
+from .db import Base
+
+class Offer(Base):
+    __tablename__ = 'offers'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Numeric, nullable=False)
+    stock = Column(Integer, nullable=False, default=0)
+    photo_url = Column(String)
+    expires_at = Column(DateTime(timezone=True))
+    product_expire_date = Column(Date)
